370f76f85acf3f0a064f944462a3417a50ad4852
yusuf0317 <mhmdy5p0317@gmail.com>
2022-04-07 22:03:00 +0700

    update

--- components/What.tsx
+++ components/What.tsx
@@ -86,22 +86,22 @@ const Program = () => {
       <div className="mt-20 w-[1024px]">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-5 text-center">Program Qurro_official.id</h1>
         <div className="flex justify-center flex-wrap">
-          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4">
+          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-600 hover:text-white transition-all duration-1000 rounded-lg">
             <p className="text-center font-bold text-2xl my-3">Tahsin Tajwid</p>
             <p className="h-28">Melancarkan bacaan Al Qur'an dan mempelajari hukum hukumnya</p>
             <button className="lg:mt-11 mt-2 px-3 py-1 text-white rounded-md bg-gradient-to-r from-blue-600 to-blue-900 hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-600">Daftar</button>
           </div>
-          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4">
+          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-600 hover:text-white transition-all duration-1000 rounded-lg">
             <p className="text-center font-bold text-2xl my-3">Tahfidz</p>
             <p className="h-28">Menghafal Al Qur'an hafalan baru dengan target 1-2 halaman perhari</p>
             <button className="lg:mt-11 mt-2 px-3 py-1 text-white rounded-md bg-gradient-to-r from-blue-600 to-blue-900 hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-600">Daftar</button>
           </div>
-          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4">
+          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-600 hover:text-white transition-all duration-1000 rounded-lg">
             <p className="text-center font-bold text-2xl my-3">Muroja'ah</p>
             <p className="h-28">Mengulang hafalan Al Qur'an yang sudah dihafal dengan target 4-5 halaman perhari</p>
             <button className="lg:mt-11 mt-2 px-3 py-1 text-white rounded-md bg-gradient-to-r from-blue-600 to-blue-900 hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-600">Daftar</button>
           </div>
-          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4">
+          <div className="text-center px-5 py-5 shadow-lg lg:w-[223px] md:w-[380px] sm:w-[500px] lg:h-[300px] h-[250px] mx-4 mb-4 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-600 hover:text-white transition-all duration-1000 rounded-lg">
             <p className="text-center font-bold text-2xl my-3">Bahasa Arab</p>
             <p className="h-28">Mempelajari dasar dasarnya yang sangat cocok sekali bagi orang awam yg ingin mempelajarinya</p>
             <button className="lg:mt-11 mt-2 px-3 py-1 text-white rounded-md bg-gradient-to-r from-blue-600 to-blue-900 hover:bg-gradient-to-r hover:from-blue-900 hover:to-blue-600">Daftar</button>
